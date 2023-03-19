4d4f3b7e23a52d9a59f2b6a8cc0ace241976cdf2
Oracle Public Cloud User <opc@jenkins-master.sub06270359170.jenkinsvcn.oraclevcn.com>
2023-03-19 14:28:59 +0000

    Updated Readme.md

---
+++ auto_var.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "export WEBEX_TOKEN=NTc1ZTY1ZDQtZGMxYS00YzEyLTljMDEtMzMzM2VjNzBjZTRiNmE5MGQ5ZDUtZjM3_PF84_81a7c864-4083-4c54-8097-2c1e5ee1140a" >> ~/.bashrc
+echo "export GPT_TOKEN=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" >> ~/.bashrc
+echo "export WEBEX_DOMAIN=WbxGPT@webex.bot" >> ~/.bashrc
+echo "source ~/.bashrc"
