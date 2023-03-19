6eb8f85a1873f04534c4ef167d5539f8ea34628e
name = ER!CK <38144008+ERICK-ZABALA@users.noreply.github.com>
2023-03-19 05:01:49 -0400

    Update auto_var.sh

--- auto_var.sh
+++ auto_var.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "export WEBEX_TOKEN=NTc1ZTY1ZDQtZGMxYS00YzEyLTljMDEtMzMzM2VjNzBjZTRiNmE5MGQ5ZDUtZjM3_PF84_81a7c864-4083-4c54-8097-2c1e5ee1140a" >> ~/.bashrc
-echo "export GPT_TOKEN=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" >> ~/.bashrc
-echo "export WEBEX_DOMAIN=WbxGPT@webex.bot" >> ~/.bashrc
+echo "export WEBEX_TOKEN=xxxxx" >> ~/.bashrc
+echo "export GPT_TOKEN=xxxxxx" >> ~/.bashrc
+echo "export WEBEX_DOMAIN=xxxx" >> ~/.bashrc
 echo "source ~/.bashrc"
